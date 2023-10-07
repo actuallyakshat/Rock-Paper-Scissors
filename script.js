624334b0678815f02b4b3da1905f451c9546eec1
om-chauhan1 <e22cseu1437@benntt.edu.in>
2023-10-07 20:33:09 +0530

    created init function

--- script.js
+++ script.js
@@ -1,23 +1,27 @@
-const userChoice = document.querySelector("[.user-chose]");
-const compChoice = document.querySelector("[.comp-chose]");
-const rock = document.querySelector("[.button1]");
-const paper = document.querySelector("[.button1]");
-const scissor = document.querySelector("[.button1]");
-const finalResult = document.querySelector("[.result]");
-const userScore = document.querySelector("[.user-score]");
-const compScore = document.querySelector("[.comp-score]");
-const resetButton = document.querySelector("[.reset]");
+const userChoice = document.querySelector(".user-chose");
+const compChoice = document.querySelector(".comp-chose");
+const rock = document.querySelector(".button1");
+const paper = document.querySelector(".button2");
+const scissor = document.querySelector(".button3");
+const finalResult = document.querySelector(".result");
+const userScore = document.querySelector(".user-score");
+const compScore = document.querySelector(".comp-score");
+const resetButton = document.querySelector(".reset");
 
-function remove(){
-    button1.classList.remove("active");
-    button2.classList.remove("active");
-    button3.classList.remove("active");
+function removeActive(){
+    rock.classList.remove('active');
+    paper.classList.remove('active');
+    scissor.classList.remove('active');
 }
 
 function init(){
     userChoice.innerText = "";
     compChoice.innerText = "";
-    remove();
+    removeActive();
+    finalResult.innerText = "Select Your Choice";
+    userScore.innerText = "";
+    compScore.innerText = "";
+    resetButton.style.display = "none";
 }
 
 init();
